1c7362910481af1fe72f567c8625a41eceae4e94
nancyhalladay24 <nancy.halladay1@nhs.net>
2021-09-06 17:11:09 +0100

    New gitpod journey

--- app/routes.js
+++ app/routes.js
@@ -3,5 +3,13 @@ const express = require('express');
 const router = express.Router();
 
 // Add your routes here - above the module.exports line
+router.post('/docs/install/install-rights', function (req, res){
+if (req.body['installrights'] === 'Yes') {
+  res.redirect ('/docs/install/mac-or-windows');
+  }
+  else {
+    res.redirect('/docs/how-tos/browser-setup-start');
+  }
+});
 
 module.exports = router;
